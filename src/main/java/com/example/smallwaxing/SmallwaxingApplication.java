c68e767c1646461665765800958d52d42c0a37a3
yunjh1219 <orenjh1219@gmail.com>
2025-07-26 13:48:44 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/smallwaxing/SmallwaxingApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallwaxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallwaxingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallwaxingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smallwaxing/SmallwaxingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallwaxing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallwaxingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
