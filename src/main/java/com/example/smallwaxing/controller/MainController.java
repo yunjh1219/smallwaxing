3dff94b605285e1a3cc46da05ed670cec1ccad54
orenjh1219 <yunjh1219@gmail.com>
2025-07-27 11:23:13 +0900

    init : 디렉토리 생성

--- src/main/java/com/example/smallwaxing/controller/MainController.java
+++ src/main/java/com/example/smallwaxing/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.example.smallwaxing.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-
+    @GetMapping("/")
+    public String main(){
+        return "pages/main";
+    }
 
 }
 
